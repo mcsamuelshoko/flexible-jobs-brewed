2273047af2144e27df25ebc3d161d76a868d496b
Mc Samuel Shoko <66551316+mcsamuelshoko@users.noreply.github.com>
2025-05-02 09:08:12 +0200

    update: added controller routes

--- web/lib/flexible_jobs_brewed_web/controllers/page_controller.ex
+++ web/lib/flexible_jobs_brewed_web/controllers/page_controller.ex
@@ -6,4 +6,24 @@ defmodule FlexibleJobsBrewedWeb.PageController do
     # so skip the default app layout.
     render(conn, :home, layout: false)
   end
+
+  def about(conn, _params) do
+    render(conn, :about, layout: false)
+  end
+
+  def contact(conn, _params) do
+    render(conn, :contact, layout: false)
+  end
+
+  def privacy(conn, _params) do
+    render(conn, :privacy, layout: false)
+  end
+
+  def terms(conn, _params) do
+    render(conn, :terms, layout: false)
+  end
+
+  def jobs(conn, _params) do
+    render(conn, :jobs, layout: false)
+  end
 end
