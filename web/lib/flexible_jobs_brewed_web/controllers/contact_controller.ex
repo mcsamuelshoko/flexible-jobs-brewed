61564b1c631a295e3752fdd62f66901e72992c85
Mc Samuel Shoko <66551316+mcsamuelshoko@users.noreply.github.com>
2025-05-04 00:26:47 +0200

    feat: added /contact route
    create contact_controller.ex, contact_html.ex and web/lib/flexible_jobs_brewed_web/views/contact_html/index.html.heex

---
+++ web/lib/flexible_jobs_brewed_web/controllers/contact_controller.ex
@@ -0,0 +1,10 @@
+defmodule FlexibleJobsBrewedWeb.ContactController do
+  use FlexibleJobsBrewedWeb, :controller
+
+  def index(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :index, layout: false)
+  end
+
+end
---
+++ web/lib/flexible_jobs_brewed_web/views/contact_html.ex
@@ -0,0 +1,10 @@
+defmodule FlexibleJobsBrewedWeb.ContactHTML do
+  @moduledoc """
+  This module contains pages rendered by PageController.
+
+  See the `page_html` directory for all templates available.
+  """
+  use FlexibleJobsBrewedWeb, :html
+
+  embed_templates "contact_html/*"
+end
