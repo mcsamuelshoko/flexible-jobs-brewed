2bdbb8a43dfd4d0a11cd2a21046278f555ca1163
Mc Samuel Shoko <66551316+mcsamuelshoko@users.noreply.github.com>
2025-05-04 00:09:02 +0200

    update flexible_jobs_brewed_web.ex

--- web/lib/flexible_jobs_brewed_web.ex
+++ web/lib/flexible_jobs_brewed_web.ex
@@ -17,7 +17,7 @@ defmodule FlexibleJobsBrewedWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(assets bootstrap fonts images favicon.ico favicon.svg robots.txt)
 
   def router do
     quote do
