7b80e0b93e9f084335f532f301fe42695f95aba2
Mc Samuel Shoko <66551316+mcsamuelshoko@users.noreply.github.com>
2025-05-04 22:14:09 +0200

    update contact_controller.ex

--- web/lib/flexible_jobs_brewed_web/controllers/contact_controller.ex
+++ web/lib/flexible_jobs_brewed_web/controllers/contact_controller.ex
@@ -2,9 +2,12 @@ defmodule FlexibleJobsBrewedWeb.ContactController do
   use FlexibleJobsBrewedWeb, :controller
 
   def index(conn, _params) do
-    # The home page is often custom made,
+    # The index page is often custom made,
     # so skip the default app layout.
     render(conn, :index, layout: false)
   end
 
+  def show(conn, %{"messenger" => messenger} = _params) do
+    render(conn, :show, messenger: messenger)
+  end
 end
