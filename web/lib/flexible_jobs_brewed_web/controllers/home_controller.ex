e2076c22b5ec928f1571fcba658573ca4f9db48b
Mc Samuel Shoko <66551316+mcsamuelshoko@users.noreply.github.com>
2025-05-25 07:56:57 +0200

    update home_controller.ex

--- web/lib/flexible_jobs_brewed_web/controllers/home_controller.ex
+++ web/lib/flexible_jobs_brewed_web/controllers/home_controller.ex
@@ -1,11 +1,6 @@
 defmodule FlexibleJobsBrewedWeb.HomeController do
   use FlexibleJobsBrewedWeb, :controller
 
-  def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
-  end
 
   def index(conn, _params) do
     # The home page is often custom made,
