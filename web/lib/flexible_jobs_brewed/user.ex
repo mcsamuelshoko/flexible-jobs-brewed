4e309ef236c842f98ea1dadf2b5ec27c41b2a8db
Mc Samuel Shoko <66551316+mcsamuelshoko@users.noreply.github.com>
2025-05-28 20:41:36 +0200

    feat: create user.ex

---
+++ web/lib/flexible_jobs_brewed/user.ex
@@ -0,0 +1,21 @@
+defmodule FlexibleJobsBrewed.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :name, :string
+    field :email, :string
+    field :bio, :string
+    field :number_of_pets, :integer
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:name, :email, :bio, :number_of_pets])
+    |> validate_required([:name, :email, :bio, :number_of_pets])
+    |> unique_constraint(:email)
+  end
+end
